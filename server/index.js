fa5d0cc97307d8ace12a8553f1490387c0f7e35e
Flarp <flurpdadurp@gmail.com>
2018-03-30 12:02:46 -0400

    no more strength

--- server/index.js
+++ server/index.js
@@ -2,38 +2,27 @@ const ws = require('ws')
 const NO_WORK = '2497f9db0a3e8a3f424642dcbb000daf39c935ec-distri'
 
 class Distri extends ws.Server {
-    constructor(args, file, strength) {
+    constructor(args, file) {
         super(args)
 
-        this.session = new Map()
-        this.available = new Set()
+        this.session = new Set()
         this.waiting = new Set()
-        this.strength = strength
 
         this.on('connection', socket => {
             socket.work = NO_WORK
 
             socket.send(JSON.stringify({ file }));
             socket.on('message', message => {
-                // if the user has no work, there's no point
-                // checking their result
+                
                 if (this.session.has(socket.work)) {
                     const work = socket.work
-                    const temp = this.session.get(work)
-                    const solution = JSON.parse(message)
-                    temp.workers--
-                    temp.solutions.push(solution)
-                    if (temp.solutions.length === strength) {
-                        this.emit('workgroup_complete', work, temp.solutions)
-                        this.session.delete(work)
-
-                        // anticipating edge case where strength is 1
-                        this.available.delete(work)
-                        if (this.session.size === 0) {
-                            this.emit('all_work_complete')
-                        }
-                    } else {
-                        this.session.set(work, temp)
+                    let solution
+                    try {
+                        solution = JSON.parse(solution)
+                        this.emit('workgroup_complete', work, solution)
+                        if (this.session.size === 0) this.emit('all_work_complete')
+                    } catch (e) {
+                        
                     }
                 }
                 this.serveSocket(socket)
@@ -41,11 +30,8 @@ class Distri extends ws.Server {
 
             socket.on('close', _ => {
                 this.waiting.delete(socket)
-                if (this.session.has(socket.work)) {
-                    const temp = this.session.get(current_work)
-                    temp.workers--
-                    this.session.set(current_work, temp)
-                    this.available.add(current_work)
+                if (socket.work !== NO_WORK) {
+                    this.session.add(socket.work)
                 }
                 this.serveTheHungry()
             })
@@ -53,28 +39,18 @@ class Distri extends ws.Server {
     }
 
     addWork(work) {
-        work.map(entry => {
-            this.session.set(entry, { workers: 0, solutions: [] })
-            this.available.add(entry)
-        })
+        work.map(entry => this.session.add(entry))
         this.serveTheHungry()
     }
 
     serveSocket(socket) {
-        if (this.available.size > 0) {
-            socket.work = Array.from(this.available)[Math.floor(Math.random() * this.available.size)]
-            console.log(socket.work)
-            const temp = this.session.get(socket.work)
-            if (( temp.workers + temp.solutions.length) === this.strength) {
-                this.available.delete(last_work)
-            }
-            temp.workers++
-            this.session.set(socket.work, temp)
+        if (this.session.size > 0) {
+            socket.work = Array.from(this.session)[Math.floor(Math.random() * this.session.size)]
+            this.session.delete(socket.work)
             socket.send(JSON.stringify(socket.work))
         } else {
             socket.work = NO_WORK
             this.waiting.add(socket)
-            console.log('hih')
         }
     }
 
