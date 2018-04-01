ff0d13eded57119c14accb30d7af639016b04c7a
Flarp <flurpdadurp@gmail.com>
2018-04-01 18:55:51 -0400

    just the gui for today

--- client/distri.js
+++ client/distri.js
@@ -1,23 +1,78 @@
+(function(){
 const storage = window.localStorage
-const thing = []
-for (let x = 0; x < storage.length; x++) {
-    thing[x] = `<b>${storage.key(x)}</b>`
+const max = window.navigator.hardwareConcurrency
+window.Distri = window.Distri ? window.Distri : {}
+let avail = window.Distri.avail = max
+let stored = storage.getItem('distri-saved')
+
+if (!stored) {
+    window.Distri.saved = {}
+} else {
+    window.Distri.saved = JSON.parse(stored)
 }
 
-const main = `
-<div class="modal fade" id="distri-main">
+saved = window.Distri.saved
+
+const ret = []
+let x = 0
+for (const key in saved) {
+    const x_floor = Math.floor(x/3)
+    if(!ret[x_floor]) ret[x_floor] = []
+    if ((avail - saved[key].workers) > 0) {
+        avail -= saved[key].workers
+    } else {
+        saved[key].workers = 0
+    }
+    const temp = { ...saved[key], link: key }
+    ret[x_floor].push(temp) 
+    x++  
+}
+
+
+const main = document.createElement('div')
+main.id = 'distri-main'
+main.innerHTML = `
+<div id="distri-inside" class="modal fade">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h4>Distri Menu</h4>
             </div>
             <div class="modal-body">
-                <p>${thing.join()}</p>
+                ${ret.map(row => 
+                    `<div class="row">
+                        ${row.map(item => 
+                            `<div class="col-sm-${12/row.length}">
+                             <center>
+                                <h5>${item.title}</h5>
+                                <h6 id='distri-workers-${item.link}'>${item.workers}</h6>
+                                <button class="btn btn-danger" onClick='
+                                    let x = window.Distri.saved[${JSON.stringify(item.link)}]
+                                    if (x.workers > 0) {
+                                        x.workers--
+                                        document.getElementById("distri-workers-${item.link}").innerText = x.workers
+                                    }
+                                    '>-</button>
+                                <button class="btn btn-success" onClick='
+                                    let x = window.Distri.saved[${JSON.stringify(item.link)}]
+                                    if (window.Distri.avail > 0) {
+                                        x.workers++
+                                        window.Distri.avail--
+                                        document.getElementById("distri-workers-${item.link}").innerText = x.workers
+                                    }
+                                '>+</button>
+                                <center>
+                            </div>`
+                        ).join('')}
+                    </div>`
+                ).join('')}
             </div>
         </div>
     </div>
 </div>
 `
 
-document.body.innerHTML = main
-$("#distri-main").modal()
+document.body.appendChild(main)
+$('#distri-inside').modal()
+
+})()
